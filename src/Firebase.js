13d1ad4089e3128397acead1428a49077cd0d18e
Harsh <valambeharsh1701@gmail.com>
2023-09-21 21:38:54 +0530

    firebase installed

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,13 @@
+console.log(import.meta.env.VITE_apikey)
+
 
 export const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId:process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: "AIzaSyD9Wb6xXLhGJ4DWXsXLagakiHH7z0cuaZY",
+  authDomain: "legalease-bb0ad.firebaseapp.com",
+  projectId:"legalease-bb0ad",
+  storageBucket: "legalease-bb0ad.appspot.com",
+  messagingSenderId: "7173006259",
+  appId: "1:7173006259:web:b114b0b40aa989ae34a1c6"
 };
 
 
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,14 @@
 import {getAuth, GoogleAuthProvider,signInWithPopup} from "firebase/auth"
 import { firebaseConfig } from "../Firebase";
 import { initializeApp } from "firebase/app";
+
+const app = initializeApp(firebaseConfig);
+
+const auth=getAuth(app);
+const provider=new GoogleAuthProvider()
+
 export default function SignIn() {
-    const app = initializeApp(firebaseConfig);
-    const auth=getAuth(app);
-    const provider=new GoogleAuthProvider()
+    // console.log("ENV LOADED: ", firebaseConfig.apiKey)
     
     const signIn=()=>{
         signInWithPopup(auth,provider).then((result)=>{
