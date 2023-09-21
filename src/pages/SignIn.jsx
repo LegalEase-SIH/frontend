ff97c243ca87a9a8af9667fae6098db27ed92ba8
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-09-22 00:48:33 +0530

    Feat:Chat completed chat backend intergration

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import {getAuth, GoogleAuthProvider,signInWithPopup} from "firebase/auth"
 import { firebaseConfig } from "../Firebase";
 import { initializeApp } from "firebase/app";
+import { useNavigate } from "react-router-dom";
 
 const app = initializeApp(firebaseConfig);
 
@@ -9,14 +10,33 @@ const provider=new GoogleAuthProvider()
 
 export default function SignIn() {
     // console.log("ENV LOADED: ", firebaseConfig.apiKey)
-    
+    const navigate=useNavigate()
     const signIn=()=>{
         signInWithPopup(auth,provider).then((result)=>{
-            console.log(result);
+            //  console.log(result);
+             getAuth().currentUser.getIdToken().then(function(idToken){
+               console.log(idToken)
+    
+               navigate("/")
+             })
         }).catch((error)=>{
             console.log(error);
         })
+        
+        let userId
+        auth.onAuthStateChanged((user)=>{
+            if(user){
+                console.log("userId: ",user.uid)
+                userId=user.uid
+              
+            }
+            else {
+                console.log("User not logged in")
+            }
+        })
     }
+
+    
 
     return (
 
