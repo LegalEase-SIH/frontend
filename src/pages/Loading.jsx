779f5ef0bc6fb1fe87d7299d93190488c9644c97
Harsh <valambeharsh1701@gmail.com>
2023-09-23 02:26:23 +0530

    idk

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,63 @@
+import Lottie from "react-lottie";
+
+import * as location from "../earth.json";
+import * as success from "../succeess_animation.json";
+import { useEffect, useState } from "react";
+function Loading() {
+  const defaultOptions1 = {
+    loop: true,
+    autoplay: true,
+    animationData: location.default,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const defaultOptions2 = {
+    loop: true,
+    autoplay: true,
+    animationData: success.default,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const [data, setData] = useState([]);
+  const [loading, setloading] = useState(undefined);
+  const [completed, setcompleted] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setloading(true);
+          setTimeout(() => {
+            setcompleted(true);
+          }, 1000);
+        });
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      {!completed ? (
+        <>
+          {!loading ? (
+            <Lottie options={defaultOptions1} height={200} width={200} />
+          ) : (
+            <Lottie options={defaultOptions2} height={100} width={100} />
+          )}
+        </>
+      ) : (
+        <>
+          <div className="font-bold text-xl">Successfully Done!!!</div>
+        </>
+      )}
+    </>
+  );
+}
+
+export default Loading;
--- src/pages/Neroutput.jsx
+++ src/pages/Neroutput.jsx
@@ -1,8 +1,58 @@
-export default function Neroutput() {
-    return (
-        <h1 className="text-3xl font-bold text-bg-font underline pl-24 pr-24">
-            Named entity recognition Result
-        </h1>
+import NerContainer from "../components/NerContainer";
 
-    )
+export default function Neroutput() {
+  return (
+    <div className="pl-24 pr-24">
+      <h1 className="text-3xl font-bold text-bg-font underline ">
+        Named entity recognition Result
+      </h1>
+      <div className="flex  justify-between mt-5 mb-5">
+        <NerContainer
+          WITNESS="w"
+          OTHER_PERSON="o"
+          STATUTE="s"
+          CASE_NUMBER="c"
+          GPE="g"
+          ORG="o"
+          DATE="d"
+          JUDGE="j"
+          PROVISION="pv"
+          PETITIONER="pt"
+          RESPONDENT="r"
+          COURT="c"
+          PRECEDENT="pr"
+        />
+        <NerContainer
+          WITNESS="w"
+          OTHER_PERSON="o"
+          STATUTE="s"
+          CASE_NUMBER="c"
+          GPE="g"
+          ORG="o"
+          DATE="d"
+          JUDGE="j"
+          PROVISION="pv"
+          PETITIONER="pt"
+          RESPONDENT="r"
+          COURT="c"
+          PRECEDENT="pr"
+        />
+        <NerContainer
+          WITNESS="w"
+          OTHER_PERSON="o"
+          STATUTE="s"
+          CASE_NUMBER="c"
+          GPE="g"
+          ORG="o"
+          DATE="d"
+          JUDGE="j"
+          PROVISION="pv"
+          PETITIONER="pt"
+          RESPONDENT="r"
+          COURT="c"
+          PRECEDENT="pr"
+        />
+      </div>
+    </div>
+  );
 }
