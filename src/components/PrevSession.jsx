49c95abbc5c22c7dcf0e8fe19bf7bcef78ece5fc
Harsh <valambeharsh1701@gmail.com>
2023-09-22 15:07:08 +0530

    new chat added-f

--- src/components/PrevSession.jsx
+++ src/components/PrevSession.jsx
@@ -1,7 +1,7 @@
 export default function PrevSession({ sessionname }) {
   return (
     <div>
-      <h1 className="flex justify-center items-center text-lg font-semibold text-bg-font h-10 pl-24 pr-24 border border-slate-500 rounded-lg mb-5 p-2">
+      <h1 className="flex justify-center items-center text-lg font-semibold text-white h-10 pl-24 pr-24 border border-slate-500 rounded-lg mb-5 p-2">
         {sessionname}
       </h1>
     </div>
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -6,127 +6,130 @@ import { useParams } from "react-router-dom";
 
 const Chat = () => {
   const [input, setInput] = useState("");
-  const [sessionId, setSessionId] = useState("")
+  const [sessionId, setSessionId] = useState("");
   const [messages, setMessages] = useState([
     { text: "Hello, how can I assist you?", sender: "bot" },
   ]);
-  const [userSessions, setUserSessions] = useState([{
-    "_id": "",
-    "sessionName": ""
-  }])
+  const [userSessions, setUserSessions] = useState([
+    {
+      _id: "",
+      sessionName: "",
+    },
+  ]);
 
   let { userId } = useParams();
 
   // let latestSessionId;
-  const displayname = localStorage.getItem('name')
-  console.log("Name of the user: ", displayname)
+  const displayname = localStorage.getItem("name");
+  console.log("Name of the user: ", displayname);
 
-  const userPhotoUrl = localStorage.getItem('photoUrl')
-  console.log("User photo Url: ", userPhotoUrl)
-
-
-
+  const userPhotoUrl = localStorage.getItem("photoUrl");
+  console.log("User photo Url: ", userPhotoUrl);
 
   const handleSubmit = async (e) => {
-
     e.preventDefault();
 
     try {
       const ques = {
-        'userId': userId,
-        'question': input
-      }
-      console.log("Session-Id: ", sessionId)
+        userId: userId,
+        question: input,
+      };
+      console.log("Session-Id: ", sessionId);
 
-      // putting the user question into the database 
-      const res = await axios.put(`http://localhost:8000/api/session/${sessionId}`, ques)
-      console.log(res)
+      // putting the user question into the database
+      const res = await axios.put(
+        `http://localhost:8000/api/session/${sessionId}`,
+        ques
+      );
+      console.log(res);
 
       // fetching the user chats from the database
-      const res1 = await axios.get(`http://localhost:8000/api/session/${sessionId}`)
-      console.log("current session: ", res1.data.chats)
-      setMessages(res1.data.chats)
-      setInput("")
-
-    }
-    catch (err) {
-      console.log(err)
+      const res1 = await axios.get(
+        `http://localhost:8000/api/session/${sessionId}`
+      );
+      console.log("current session: ", res1.data.chats);
+      setMessages(res1.data.chats);
+      setInput("");
+    } catch (err) {
+      console.log(err);
     }
   };
 
   const getSessionQuestionAnswers = async () => {
     try {
-      const res = await axios.get(`http://localhost:8000/api/session/${sessionId}`)
-      console.log("current session: ", res.data.chats)
-      setMessages(res.data.chats)
+      const res = await axios.get(
+        `http://localhost:8000/api/session/${sessionId}`
+      );
+      console.log("current session: ", res.data.chats);
+      setMessages(res.data.chats);
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
+  };
 
   const createNewSession = async () => {
     const newSession = {
-      "userId": getAuth().currentUser.uid,
-      "sessionName": "New session"
-    }
+      userId: getAuth().currentUser.uid,
+      sessionName: "New session",
+    };
 
-    const res = await axios.post("http://localhost:8000/api/session", newSession)
-    console.log(res)
-  }
-
+    const res = await axios.post(
+      "http://localhost:8000/api/session",
+      newSession
+    );
+    console.log(res);
+  };
 
   const getUserSession = async () => {
     try {
-      const res = await axios.get(`http://localhost:8000/api/session/user/${userId}`)
+      const res = await axios.get(
+        `http://localhost:8000/api/session/user/${userId}`
+      );
       // console.log("User sessions: ",res.data.slice(-6))
-      const prevUserSessions = res.data.slice(-6)
-      setUserSessions(prevUserSessions)
-      console.log("userSessions: ", userSessions)
-      const latestSessionId = res.data[res.data.length - 1]._id
-      setSessionId(latestSessionId)
+      const prevUserSessions = res.data.slice(-6);
+      setUserSessions(prevUserSessions);
+      console.log("userSessions: ", userSessions);
+      const latestSessionId = res.data[res.data.length - 1]._id;
+      setSessionId(latestSessionId);
       // console.log("Session-Id: ",latestSessionId)
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
-
+  };
 
   useEffect(() => {
     // creating a new chat session
-    createNewSession()
-    getUserSession()
-    getSessionQuestionAnswers()
-
-
-  }, [])
+    createNewSession();
+    getUserSession();
+    getSessionQuestionAnswers();
+  }, []);
 
   useEffect(() => {
-
     // Scroll to the bottom of the chat when new messages are added
     const chatContainer = document.getElementById("chat-container");
     chatContainer.scrollTop = chatContainer.scrollHeight;
   }, [messages]);
 
   return (
-    <div className="flex justify-center bg-bg-light items-start h-screen">
-      {userSessions.map((userSession, index) => (
-        <div className=" bg-bg-light  p-4 w-1/4 ">
-          <PrevSession sessionname={userSession.sessionName} />
+    <div className="flex justify-center bg-bg-light  h-screen">
+      <div className="bg-bg-font">
+        <div className="flex flex-col p-8">
+          <button className="border border-slate-400 p-2 text-white rounded-lg">New Chat</button>
+          {userSessions.map((userSession, index) => (
+            <div className="p-4">
+              <PrevSession sessionname={userSession.sessionName} />
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
 
-      <div className=" bg-bg-light p-4 w-3/4">
+      <div className=" bg-bg-box p-4 w-3/4">
         <div
           id="chat-container"
           className="bg-white rounded-lg shadow-lg p-4 h-96 overflow-y-auto"
         >
           {messages.map((message, index) => (
-            <div
-              key={index}
-              className={`flex flex-col mb-2`}
-            >
+            <div key={index} className={`flex flex-col mb-2`}>
               <div
                 className={`py-2 px-3 rounded-lg inline-block break-words text-right`}
               >
