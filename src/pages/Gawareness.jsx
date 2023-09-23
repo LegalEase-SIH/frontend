72e5fad4308e64c8129a1f5b4d709b0437831561
NimishKashyap <kashyap.nimish8@gmail.com>
2023-09-23 07:36:55 +0530

    ui done

--- src/pages/Gawareness.jsx
+++ src/pages/Gawareness.jsx
@@ -1,5 +1,8 @@
+import axios from "axios";
 import { useEffect, useState } from "react"
 import { FiSearch } from "react-icons/fi"
+
+const base_url = "http://d645-35-204-1-76.ngrok-free.app"
 
 const ResultTile = ({ result }) => {
     return <div className="text-sm font-normal bg-orange-100 rounded-lg px-5 py-3 shadow-lg my-4  hover:scale-105 transition-all duration-150 cursor-pointer hover:outline-slate-800">
@@ -12,33 +15,30 @@ const ResultTile = ({ result }) => {
 
 export default function Gawareness() {
     const [results, setResults] = useState([]);
+    const [input, setInput] = useState("");
 
-    useEffect(() => {
-        const res = [
-            {
-                title: "This is title",
-                text: "This is a sample text",
-                url: "https://www.google.com"
-            },
-            {
-                title: "This is title",
-                text: "This is a laksjdf;j jkjasldf jljasdf jkja;df jlafdj kjsakdfjalf lkasdfkjasldf lasjdflkjaflj ljasldkfjasdf flkjsdkfjlkjf jlsjdfkjsldfj jljsdkfjsdf jljlksjdfsd  akljfaskfjda jkjsldfj lkdfksdjf lsjdflksdf  ljlkjfksjdf jkdflksdf ll kajaj text",
-                url: "https://www.google.com"
-            },
-            {
-                title: "This is title",
-                text: "This is a sample text",
-                url: "https://www.google.com"
-            },
-        ]
-        setResults(res)
-    }, [])
+    const handleSubmit = async (e) => {
+        // e.preventDefault();
+        try {
+            const res = await axios.get(`http://localhost:8000/api/search?query=${input}`)
+            setResults(res.data.result)
+
+        } catch (err) {
+            console.log(err)
+        }
+        // const data = await res.json();
+    }
+    // useEffect(() => {
+
+    //     setResults(res)
+    // }, [])
     return (
         <>
             <div className="text-3xl font-bold justify-self-stretch flex-1 px-5 mt-8">
                 <div className="flex items-center border-2 text-sm lg:w-2/6 px-5 py-1 my-2 focus-within:outline-gray-200 focus-within:bg-white rounded-xl shadow-sm hover:shadow-md transition-all ease-in-out duration-200">
                     <FiSearch size={20} className="text-gray-700" />
-                    <input type="text" className="ml-2 px-3 py-2 focus:border-none focus:outline-none bg-transparent" placeholder="Search for document" />
+                    <input type="text" className="ml-2 px-3 py-2 focus:border-none focus:outline-none bg-transparent" placeholder="Search for document" onChange={(e) => setInput(e.target.value)} />
+                    <button onClick={handleSubmit}>Search</button>
                 </div>
                 {results.map((result, index) => <ResultTile key={index} result={result} />)}
             </div>
